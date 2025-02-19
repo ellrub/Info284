0df3dd20223cf529c73dfedb773e3cfb562f5681
Ivan Mitrev <ivmit0542@uib.no>
2025-02-19 09:59:22 +0100

    implement DATASET_REDUXXION.py (thing that reduces a csv to a smaller subset)

---
+++ utility/DATASET_REDUXXION.py
@@ -0,0 +1,7 @@
+import pandas;
+import random;
+
+# REDUCES CSV TO A SMALLER SUBSET OF THE CSV AND MAKES A NEW CSV
+def csvReduxxion(_sPath, _sPathNew, _nSizeNew, _nRandomState, _sEncoding = "utf-8"):
+    _dataset = pandas.read_csv(_sPath);
+    _dataset.sample(n = _nSizeNew, random_state = _nRandomState).to_csv(_sPathNew, encoding = _sEncoding);
