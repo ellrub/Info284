7205f2b12e7ccd72b5bfd353cdd0616b68648569
snorting-off-yr-moms-ass <ivansbruker@gmail.com>
2025-04-02 08:59:34 +0200

    Update NEURAL.py
    ok

--- neural_network_anal(ys)/NEURAL.py
+++ neural_network_anal(ys)/NEURAL.py
@@ -6,7 +6,7 @@ import pandas;
 def dataframeConverzzion(_dataset):
     _mergedReviews = [];
     for i in _dataset.to_numpy():
-        _mergedReviews.append([ i[2], i[0] + " " + i[1] ]);
+        _mergedReviews.append({ "score": i[2], "review": i[0] + " " + i[1] });
     #return pandas.DataFrame(_mergedReviews);
     return _mergedReviews;
 
@@ -23,7 +23,25 @@ dataset = dataframeConverzzion(dataset);
 
 
 #############
-# DATASET PREPARATION FUNXXIONS
+# DATASET EXTRAXXION
+
+def entryExtraxxion( _entry, _col ):
+    _entryExtracc = [];
+    for _wordExtracc in _entry[_col]:
+        _entryExtracc.append(_wordExtracc);
+    return _entryExtracc;
+
+def datasetExtraxxion( _dataset, _col):
+    _datasetExtracc = [];
+    for _entry in _dataset:
+        _entryExtracc = entryExtraxxion(_entry, _col);
+        _datasetExtracc.append(_entryExtracc);
+    return _datasetExtracc;
+
+
+
+#############
+# DATASET PREPARATION
 
 import re;
 import nltk;
@@ -33,77 +51,161 @@ nltk.download("stopwords");
 nltk.download("punkt_tab");
 nltk.download("wordnet");
 lemma = nltk.WordNetLemmatizer();
+stemma = nltk.PorterStemmer();
 
-def stringClean(_string):
+def stringClean( _string ):
     _string = _string.lower();
     _string = re.sub(r"\d+", "", _string);
     _string = re.sub(r"[^\w\s]", "", _string);
     return _string;
 
-def lemmaBalls(_string):
+def lemmaBalls( _string ):
     _text = nltk.word_tokenize(_string);
-    _text_without_trash = [];
+    _textLemma = [];
     for _word in _text:
-        if not (_word in nonWords):
-            _text_without_trash.append(lemma.lemmatize(_word));
-    return _text_without_trash;
+        _wordLemma = lemma.lemmatize(_word);
+        if not (_wordLemma in nonWords):
+            _textLemma.append(_wordLemma);
+    return _textLemma;
 
-def datasetPreparation(_dataset):
-    for _review in _dataset:
-        _review[1] = lemmaBalls(stringClean(_review[1]));
-    return _dataset;
+def stemmaBalls( _string ):
+    _text = nltk.word_tokenize(_string);
+    _textStemma = [];
+    for _word in _text:
+        _wordStemma = stemma.stem(_word);
+        if not (_wordStemma in nonWords):
+            _textStemma.append(_wordStemma);
+    return _textStemma;
+
+def datasetPreparation( _dataset, _mode =  "lemma" ):
+    _datasetPrepare = [];
+    if( _mode == "lemma" ):
+        for _review in _dataset:
+            _reviewPrepare = { "score": _review["score"], "review": lemmaBalls(stringClean(_review["review"])) };
+            _datasetPrepare.append(_reviewPrepare);
+    elif( _mode == "stemma" ):
+        for _review in _dataset:
+            _reviewPrepare = { "score": _review["score"], "review": stemmaBalls(stringClean(_review["review"])) };
+            _datasetPrepare.append(_reviewPrepare);
+    else:
+        raise ValueError("provided mode does not exist");
+
+    return _datasetPrepare;
 
 
 
 #############
 # PREPARING DATASET
 
-datasetPrepared = datasetPreparation(dataset);
+datasetPrepared = datasetPreparation(dataset, _mode = "stemma");
 
 
 
 #############
-# ENCRYPXION FUNXXION
+# DATASET VECTORIZATION
 
-# so the word fragment usually in shaders and filters and stuff refers to 
-# one point on the texture or surface or whatever you are rendering to
-# basicly like a pixel, but a little more abstract
-# im gonna be running a convolutional filter on the dataset
-# and i want each word to be one fragment for the filter
-# it kinda sucks to have spaces and words of varying lengths because then the filter
-# has to figure out dynamically where one fragment ends and begins,
-# meaning it probly has to read ahead until the next space or something and that sucks
-# so im turning words into fragments of fixed size
-# so that the filter knows that a fragment is always exactly say 4 characters
-# so then it doesnt need to figure it out on its own, it doesnt even need spaces between words
-# so then im kinda turning the words into "pixels" for the filter
+def entryVecc( _entry, _model ):
+    _entryVecc = [];
+    for _word in _entry:
+        if( _word in _model.wv ):
+            _wordVecc = _model.wv[_word];
+            _entryVecc.append(_wordVecc);
+    return _entryVecc;
 
-# put all the words into a set
-# count how many entries there are in the set
-# calculate how many digits in base 64 would be needed to uniquely identify words
-# make a relation from b64 keys to words
-# make a relation from words to b64 keys
-# replace words in dataset with b64 keys
-# remove spaces
-
-import math;
-
-def datasetEncrypxion(_dataset):
-    _wordSet = {};
+def datasetVectorization( _dataset, _model ):
+    _datasetVecc = [];
     for _review in _dataset:
-        for _word in _review:
-            _wordSet.add(_word);
-    
-    _requiredDigits = math.floor(math.log(len(_wordSet), 64)) + 1;
-    _wordList = [];
+        _reviewVecc = { "score": _review["score"], "review": entryVecc(_review["review"], _model) };
+        _datasetVecc.append(_reviewVecc);
+    return _datasetVecc;
 
-    for _word in _wordSet:
-        _wordList.add(_word);
 
-    _wordsRelateKeys = {};
-    _keysRelateWords = {};
 
-    for _i in range(len(_word)):
-        _key = 
-        _wordsRelateKeys[_]
-        
+
+#############
+# VECTORIZING DATASET
+
+from gensim.models import Word2Vec;
+
+fragmentChannels = 100;
+
+datasetExtracc = datasetExtraxxion(datasetPrepared, "review");
+
+# Create CBOW model
+bowModel = Word2Vec(datasetExtracc, min_count = 1, vector_size = fragmentChannels, window = 5);
+datasetVeccBow = datasetVectorization(datasetPrepared, bowModel);
+
+# Create Skip Gram model
+skipModel = Word2Vec(datasetExtracc, min_count = 1, vector_size = fragmentChannels, window = 5, sg = 1);
+datasetVeccSkip = datasetVectorization(datasetPrepared, skipModel);
+
+
+
+#############
+# SPLITTING DATASET
+
+from sklearn.model_selection import train_test_split
+
+dataframeVeccBow = pandas.DataFrame(datasetVeccBow);
+dataframeVeccSkip = pandas.DataFrame(datasetVeccSkip);
+
+veccBowtrainX, veccBowTexts, veccBowtrainY, veccBowTestY = train_test_split(dataframeVeccBow["review"].values, dataframeVeccBow["score"].values, test_size = 0.20, random_state = 1);
+veccSkiptrainX, veccSkipTestX, veccSkiptrainY, veccSkipTestY = train_test_split(dataframeVeccSkip["review"].values, dataframeVeccSkip["score"].values, test_size = 0.20, random_state = 1);
+
+print(veccSkiptrainX[0]);
+print(veccSkipTestX[0]);
+print(veccSkiptrainY[0]);
+print(veccSkipTestY[0]);
+
+
+
+#############
+# DEFINING NETWORK
+
+import tensorflow;
+from tensorflow.keras import layers, datasets, models, callbacks;
+
+with tensorflow.device("/GPU:0"):
+    model = models.Sequential();
+    model.add(layers.Input( (None, fragmentChannels)) );
+    model.add(layers.Conv1D( 32, 4, activation = "relu", strides = 1 ));
+    model.add(layers.BatchNormalization());
+    model.add(layers.Dense( 96, activation = 'relu' ));
+    model.add(layers.Dropout( 0.2 ));
+    model.add(layers.Dense( 192, activation = 'relu' ));
+    model.add(layers.Dropout( 0.3 ));
+    model.add(layers.Dense( 128, activation = 'relu' ));
+    model.add(layers.Dropout( 0.2 ));
+    model.add(layers.Dense( 10, activation = 'relu' ));
+
+    model.compile( optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'] );
+
+    early_stopping = callbacks.EarlyStopping( monitor = 'val_loss', patience = 5, restore_best_weights = True );
+
+    history = model.fit(
+        veccSkiptrainX,
+        veccSkiptrainY,
+        batch_size = 64,
+        epochs = 50,
+        validation_data = (veccSkipTestX, veccSkipTestY),
+        callbacks = [early_stopping]
+    );
+
+
+# use flatten layer if youre gonna do several sentences in parrallel
+
+
+
+#############
+# VISUALIZATION /// EVALUATION 
+
+from matplotlib import pyplot;
+
+pyplot.plot(history.history['accuracy'], label='accuracy');
+pyplot.plot(history.history['val_accuracy'], label = 'val_accuracy');
+pyplot.xlabel('Epoch');
+pyplot.ylabel('Accuracy');
+pyplot.ylim([0.5, 1]);
+pyplot.legend(loc='lower right');
+
+loss, accuracy = model.evaluate(veccSkipTestX,  veccSkipTestY, verbose=2);
